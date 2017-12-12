c531c997aa8cd7f7797fa3f414342188bcc282e9
aikaramba <a.balanyuk@yahoo.com>
2017-12-12 17:31:13 +0200

    Add config helper scripts & Add utils folder to default Dockerfile build

---
+++ kubernetes/helpers/create_config_maps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#kubectl create configmap config-maps-folder-name \
+#--from-literal=config-name="config-value" \
+#--from-file=./file-name \
---
+++ kubernetes/helpers/create_secrets.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#kubectl create secret generic secret-folder-name \
+#--from-literal=secret-name="secret-value" \
+#--from-file=./file-name \
